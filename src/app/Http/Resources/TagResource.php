24911ed475e56e7f457cb1fac62d23a0e4f866ab
Ahmed Swerky <ahmdswerky@gmail.com>
2022-03-15 08:19:07 +0200

    chore: add created_at to tag resource

--- src/app/Http/Resources/TagResource.php
+++ src/app/Http/Resources/TagResource.php
@@ -17,6 +17,7 @@ class TagResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'created_at' =>  $this->created_at,
         ];
     }
 }
