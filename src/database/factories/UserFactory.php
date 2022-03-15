08c7c5ecaee4082e39e4ad8a42855588658737ae
Ahmed Swerky <ahmdswerky@gmail.com>
2022-03-15 08:17:22 +0200

    fix: remove unnessasary fields from users table

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -20,23 +20,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => 'password',
-            'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
